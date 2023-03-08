fd63bfa7f31534aaec0ec0b46c30d3472fc7487c
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-03-08 09:01:07 +0100

    feat: more projects added and styling modified

--- src/components/portfolio/portfolioData.js
+++ src/components/portfolio/portfolioData.js
@@ -4,32 +4,48 @@ import calculatorImg from '../../assets/images/calculator.png'
 import ipTrackerImg from '../../assets/images/ip-tracker.png'
 import confyImg from '../../assets/images/gadgetgalore.png'
 import githubImg from '../../assets/images/github-user.png'
+import forktifyImg from '../../assets/images/forktify.png'
+import maptyImg from '../../assets/images/mapty.png'
+import bankishImg from '../../assets/images/bankish.png'
+import bankishlpImg from '../../assets/images/bankishlp.png'
+import todoAppImg from '../../assets/images/todoApp.png'
+
 
 const projects = [
   {
     id: 1,
+    name: 'Forktify',
+    about: 'Recipe search created using (React, styled components, API)',
+    img: forktifyImg,
+    url: 'https://forktify.vercel.app/',
+    git: 'https://github.com/Demandtech/forktify-React-Js',
+  },
+  {
+    id: 2,
+    name: 'Mapty',
+    about:
+      'Sport Tracker created using (HTML, CSS, JavaScript(OOP), Leaflet libary',
+    img: maptyImg,
+    url: 'https://mapty-lyart.vercel.app/',
+    git: 'https://github.com/Demandtech/Mapty',
+  },
+  {
+    id: 9,
+    name: 'Bankish',
+    about: 'Banking App created with HTML, CSS, and JavaScript',
+    img: bankishImg,
+    url: 'https://bankish-bank.vercel.app/',
+    git: 'https://github.com/Demandtech/bankish-bank',
+  },
+  {
+    id: 3,
     name: 'Url Shortner',
     about: 'Landing page of Url Shortner created (React & Sass)',
     img: urlShortnerImg,
     url: 'https://url-shortener-brown-pi.vercel.app/',
     git: 'https://github.com/Demandtech/Url-Shortener',
   },
-  {
-    id: 2,
-    name: 'GadgetGalore E-commerce',
-    about: 'E-commerce website created with React & styled component)',
-    img: confyImg,
-    url: 'https://comfy-e-commerce.vercel.app/',
-    git: 'https://github.com/Demandtech/Comfy-E-commerce',
-  },
-  {
-    id: 3,
-    name: 'Github User',
-    about: 'Github Users search (React, styled, Api)',
-    img: githubImg,
-    url: 'https://github-user-nu.vercel.app/',
-    git: 'https://github.com/Demandtech/Github-User',
-  },
+
   {
     id: 4,
     name: 'Job Listing',
@@ -55,6 +71,39 @@ const projects = [
     img: calculatorImg,
     url: 'https://calculator-five-ruddy.vercel.app/',
     git: 'https://github.com/Demandtech/calculator',
+  },
+
+  {
+    id: 7,
+    name: 'Github User',
+    about: 'Github Users search (React, styled, Api)',
+    img: githubImg,
+    url: 'https://github-user-nu.vercel.app/',
+    git: 'https://github.com/Demandtech/Github-User',
+  },
+  {
+    id: 8,
+    name: 'GadgetGalore E-commerce',
+    about: 'E-commerce website created with React & styled component)',
+    img: confyImg,
+    url: 'https://comfy-e-commerce.vercel.app/',
+    git: 'https://github.com/Demandtech/Comfy-E-commerce',
+  },
+  {
+    id: 10,
+    name: 'Bankish bank landing page',
+    about: 'Bankish bank landing page created with HTML, Css, Javascript',
+    img: bankishlpImg,
+    url: 'https://bankish.vercel.app/',
+    git: 'https://github.com/Demandtech/Bankish',
+  },
+  {
+    id: 11,
+    name: 'Todo App',
+    about: 'Todo App created with HTML, Css, Javascript',
+    img: todoAppImg,
+    url: 'https://todo-app-one-omega-67.vercel.app/',
+    git: 'https://github.com/Demandtech/todo-app',
   },
 ]
 
